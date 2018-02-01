6cb8d104b35357b43645e824b560c432e8beb714
Nikhil B <nkl07ba@gmail.com>
2018-02-02 00:29:16 +0530

    Added new button for sign up and button for login

--- app/src/main/java/in/co/nikhil/hackernewsfirebaseapp/activity/LoginActivity.java
+++ app/src/main/java/in/co/nikhil/hackernewsfirebaseapp/activity/LoginActivity.java
@@ -1,21 +1,33 @@
 package in.co.nikhil.hackernewsfirebaseapp.activity;
 
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.common.SignInButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import in.co.nikhil.hackernewsfirebaseapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
   private static final int RC_SIGN_IN = 1;
 
   private FirebaseAuth mFirebaseAuth;
   private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+  @BindView(R.id.sign_in_button)
+  SignInButton mGoogleSignInButton;
+  @BindView(R.id.login_username)
+  TextInputEditText mLoginUserName;
+  @BindView(R.id.login_password)
+  TextInputEditText mLoginPassword;
+
 
   @Override
   protected void onStart() {
@@ -27,8 +39,13 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
 
+    // Get the firebase instance
     mFirebaseAuth = FirebaseAuth.getInstance();
+
+    // Sets the google sign in button to wide size
+    mGoogleSignInButton.setSize(SignInButton.SIZE_WIDE);
 
     // Firebase Authentication Variables
     mAuthStateListener = new FirebaseAuth.AuthStateListener() {
@@ -47,7 +64,13 @@ public class MainActivity extends AppCompatActivity {
 
   }
 
+  // Onclick method for login button
   public void checkLogin(View view) {
+
+  }
+
+  // Onclick method for Sign Up button
+  public void signUp(View view) {
 
   }
 
